2976ae26d8fe5a08c8eea3f03fc89fc2f2fdf00f
Peter van der Heijden <peter@vanderheijden.name>
2022-07-02 23:06:43 +0200

    Change to list of sweepers.

--- solver.py
+++ solver.py
@@ -12,12 +12,21 @@ class Solver:
         self.mine_field = mf.MineField(
             **{'width': width, 'height': height, 'number_of_mines': number_of_mines})
         self.grid = [[UNKNOWN] * self.width for _ in range(self.height)]
+        self._sweepers = [
+            self.sweep_middle_cell
+        ]
 
     def sweep(self) -> str:
+        for sweeper in self._sweepers:
+            result = sweeper()
+            if result:
+                return result
+
+    def sweep_middle_cell(self) -> str:
         column, row = (self.width - 1) // 2, (self.height - 1) // 2
-        if self.grid[row][column] == UNKNOWN:
-            mines = self.mine_field.sweep_cell(column, row)
-            self.grid[row][column] = mines
-            plural = 's' if mines != 1 else ''
-            return f"Sweep middle cell: ({column}, {row}) is surrounded by {mines} mine{plural}."
-        pass
+        if self.grid[row][column] != UNKNOWN:
+            return None
+        mines = self.mine_field.sweep_cell(column, row)
+        self.grid[row][column] = mines
+        plural = 's' if mines != 1 else ''
+        return f"Sweep middle cell: ({column}, {row}) is surrounded by {mines} mine{plural}."
