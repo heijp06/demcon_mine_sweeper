b77f7da0465aa9ea48beb9057825c7b512a1b52f
Peter van der Heijden <peter@vanderheijden.name>
2022-07-03 17:10:34 +0200

    Create assert_cells_swept function.

--- test_solver.py
+++ test_solver.py
@@ -7,20 +7,21 @@ def test_sweep_middle_cell():
     solver = Solver(3, 3, 1)
     solver.mine_field = MagicMock(spec=MineField)
     solver.sweep_middle_cell()
-
-    solver.mine_field.sweep_cell.assert_called_once_with(1, 1)
+    assert_cells_swept(solver, [(1, 1)])
 
 
 def test_sweep_cells_for_which_all_mines_are_found():
-    solver = Solver(3, 3, 1)
+    solver = Solver(3, 2, 1)
     solver.mine_field = MagicMock(spec=MineField)
     solver.grid = [
         [0, UNKNOWN, UNKNOWN],
         [UNKNOWN, UNKNOWN, UNKNOWN],
-        [UNKNOWN, UNKNOWN, UNKNOWN],
     ]
     solver.sweep_cells_for_which_all_mines_are_found()
+    assert_cells_swept(solver, [(1, 0), (0, 1), (1, 1)])
 
-    assert solver.mine_field.sweep_cell.call_count == 3
+
+def assert_cells_swept(solver: Solver, cells: list[tuple[int, int]]) -> None:
+    assert solver.mine_field.sweep_cell.call_count == len(cells)
     solver.mine_field.sweep_cell.assert_has_calls(
-        [call(1, 0), call(0, 1), call(1, 1)], any_order=True)
+        [call(*cell) for cell in cells], any_order=True)
