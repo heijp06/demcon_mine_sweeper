13a9a27f23640ba8450d79ecab6298673c270e8b
Peter van der Heijden <peter@vanderheijden.name>
2022-07-17 11:17:46 +0200

    Add benchmarking code.

--- main.py
+++ main.py
@@ -17,24 +17,42 @@ def print_grid(grid: list[list[int]]) -> None:
         print()
 
 
-# solver = Solver(**mf.BEGINNER_FIELD)
-# solver = Solver(**mf.INTERMEDIATE_FIELD)
-solver = Solver(**mf.EXPERT_FIELD)
-print(f"MineSweeper: {solver.width} columns, {solver.height} rows, {solver.number_of_mines} mines")
-print_grid(solver.grid)
+# # solver = Solver(**mf.BEGINNER_FIELD)
+# # solver = Solver(**mf.INTERMEDIATE_FIELD)
+# solver = Solver(**mf.EXPERT_FIELD)
+# print(f"MineSweeper: {solver.width} columns, {solver.height} rows, {solver.number_of_mines} mines")
+# print_grid(solver.grid)
 
-result = solver.sweep()
-while result:
-    print(result)
-    print_grid(solver.grid)
-    try:
-        result = solver.sweep()
-    except mf.ExplosionException:
-        print(f"Mines found at {solver.mines_found()}")
-        print("Mine exploded!!!")
-        break
-    mines = solver.mines_found()
-    if len(mines) == solver.number_of_mines:
-        print(f"Mines found at {solver.mines_found()}")
-        print(f"All mines found.")
-        break
+# result = solver.sweep()
+# while result:
+#     print(result)
+#     print_grid(solver.grid)
+#     try:
+#         result = solver.sweep()
+#     except mf.ExplosionException:
+#         print(f"Mines found at {solver.mines_found()}")
+#         print("Mine exploded!!!")
+#         break
+#     mines = solver.mines_found()
+#     if len(mines) == solver.number_of_mines:
+#         print(f"Mines found at {solver.mines_found()}")
+#         print(f"All mines found.")
+#         break
+
+wins = 0
+games = 0
+for c in range(100):
+    print(f"{c} ")
+    games += 1
+    solver = Solver(**mf.EXPERT_FIELD)
+    result = solver.sweep()
+    while result:
+        try:
+            result = solver.sweep()
+        except mf.ExplosionException:
+            break
+        mines = solver.mines_found()
+        if len(mines) == solver.number_of_mines:
+            wins += 1
+            break
+print(f"{games} games played, {wins} won ({wins * 100 / games}%).")
